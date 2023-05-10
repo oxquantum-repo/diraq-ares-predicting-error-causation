e8833ab3181537b0d313f4d044f237873bca26d6
b-vanstraat <barnaby.vanstraaten@materials.ox.ac.uk>
2023-05-10 13:54:45 +0100

    adding the ability to run on the OPX

---
+++ hhm_opx.py
@@ -0,0 +1,95 @@
+import matplotlib.pyplot as plt
+
+from opx import *
+from qm.qua import *
+from qualang_tools.loops import from_array, qua_arange
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from opx.configuration import config, host
+from time import sleep
+
+from src import Model
+
+model = Model()
+probabilities = np.array([0.5, 0.002, 0.002, 0.9, 0.9])
+model.set_probabilities(*probabilities)
+
+O_, spins = model.simulate_data(200, 1)
+
+S = np.array([0, 1])
+Pi = model.startprob_
+Tm = model.transmat_
+Em = model.emissionprob_
+
+true_alpha = forward(O_, S, Pi, Tm, Em)
+true_alpha = true_alpha / np.sum(true_alpha, axis=1).reshape(-1, 1)
+
+
+def ravel_index(n, m, shape_m):
+    return n * shape_m + m
+
+
+with program() as hmm_program:
+    N_ = len(O_)
+    M_ = 2
+
+    n = declare(int, value=0)
+    m = declare(int, value=0)
+    k = declare(int, value=0)
+
+    sum = declare(fixed, value=0.)
+    temp = declare(fixed, value=[0., 0.])
+    recip = declare(fixed, value=0.)
+
+    results_stream = declare_stream()
+
+    N = declare(int, value=len(O_))
+    M = declare(int, value=2)
+
+    O = declare(int, value=O_.astype(int).tolist())
+    Em = declare(fixed, value=Em.flatten().tolist())
+    Tm = declare(fixed, value=Tm.flatten().tolist())
+
+    alpha = declare(fixed, size=M_ * N_)
+    assign(alpha[ravel_index(0, 0, M)], Pi[0] * Em[ravel_index(0, O[0], M)])
+    assign(alpha[ravel_index(0, 1, M)], Pi[1] * Em[ravel_index(1, O[0], M)])
+
+    with for_(*qua_arange(n, 1, N_, 1)):
+        with for_(*qua_arange(m, 0, M_, 1)):
+            assign(sum, 0.)
+            with for_(*qua_arange(k, 0, M_, 1)):
+                assign(sum, sum + alpha[ravel_index(n - 1, k, M)] * Tm[ravel_index(k, m, M)])
+            assign(temp[m], sum * Em[ravel_index(m, O[n], M)])
+
+        assign(recip, 1. / (temp[0] + temp[1]))
+        assign(alpha[ravel_index(n, 0, M)], temp[0] * recip)
+        assign(alpha[ravel_index(n, 1, M)], temp[1] * recip)
+
+    with for_(m, 0, m < 2 * N, m + 1):
+        save(alpha[m], results_stream)
+
+    with stream_processing():
+        results_stream.buffer(2 * N_).save("alpha")
+
+qmm = QuantumMachinesManager(host=host)
+qm = qmm.open_qm(config=config)
+
+job = qm.execute(hmm_program)
+
+while job.result_handles.is_processing():
+    sleep(0.01)
+
+alpha = job.result_handles.get("alpha").fetch_all().reshape(-1, 2)
+alpha = alpha / np.sum(alpha, axis=1).reshape(-1, 1)
+
+print("the arrays are the same {}".format(np.isclose(alpha, true_alpha).all()))
+print(np.abs(alpha - true_alpha).max())
+
+plt.plot(1 - spins, label='True hidden spin state')
+plt.plot(1 - O_, label='Measured spin state')
+
+plt.plot(true_alpha[:, 0], label='Calculated on Lab PC', alpha=0.5)
+plt.plot(alpha[:, 0], label='Calculated OPX')
+plt.xlabel('Number of measurements')
+plt.ylabel('P(Nth spin even|measurements)')
+plt.legend()
+plt.show()
---
+++ scaling.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+
+from opx import *
+from src import Model
+
+# model = Model()
+# probabilities = np.array([0.95, 0.02, 0.02, 0.99, 0.99])
+# model.set_probabilities(*probabilities)
+#
+# O, spins = model.simulate_data(10, 1)
+
+plt.figure('Forward')
+
+ep = 1e-2
+
+for tp in np.geomspace(1e-6, 1e-1, 6):
+    O = np.zeros(15)
+
+    S = np.array([0, 1])
+    Pi = np.array([0.95, 0.5])
+    Tm = np.array([
+        [1 - tp, tp],
+        [tp, 1 - tp]
+    ])
+    Em = np.array([
+        [1 - ep, ep],
+        [ep, 1 - ep]
+    ])
+
+    P = forward(O, S, Pi, Tm, Em)
+
+    label = "$\log_{10} P($even$\\rightarrow$odd)$ =$" + "{}".format(np.log10(tp))
+    plt.plot(100 * (1 - P[:, 0]), label =label)
+
+
+plt.yscale('log')
+plt.xlabel('Number of measurements')
+plt.ylabel('Active initialisation infidelity (%)')
+plt.legend()
+plt.show()
